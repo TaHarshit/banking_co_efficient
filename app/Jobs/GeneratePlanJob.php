646b9f735b0854668f359af505fd8fc3a87fb40c
TaHarshit <ta.lhc5922@gmail.com>
2026-06-15 14:48:28 +0530

    feat: implement GeneratePlanJob for asynchronous negotiation action plan generation via external AI service

--- app/Jobs/GeneratePlanJob.php
+++ app/Jobs/GeneratePlanJob.php
@@ -136,6 +136,7 @@ class GeneratePlanJob implements ShouldQueue
                 ]);
 
                 // Notify user 
+                Log:info("user id is ".$this->userId);
                 
                 General::sendNotificationV1(
                     $this->userId,
